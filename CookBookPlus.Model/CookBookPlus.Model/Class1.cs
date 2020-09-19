ecd2017cdf02f08b0c0014eb12804ab5bd2d3276
Shane <perrysm98@gmail.com>
2020-09-18 19:25:33 -0500

    Added class library and updated Database Diagram with data types

---
+++ CookBookPlus.Model/CookBookPlus.Model/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CookBookPlus.Model
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string EmailAddress { get; set; }
+        public string Country { get; set; }
+
+    }
+
+    public class UserRatingInfo
+    {
+        public int UserRatingId { get; set; }
+        public int UserId { get; set; }
+        public int UserRanking { get; set; }
+        public string UserComments { get; set; }
+    }
+
+    public class Recipes
+    {
+        public int RecipeId { get; set; }
+        public int UserId { get; set; }
+        public string RecipeName { get; set; }
+        public string RecipeDirections { get; set; }
+        public int Protein { get; set; }
+        public int Carbs { get; set; }
+        public int Fats { get; set; }
+    }
+
+    public class RecipeRatingInfo
+    {
+        public int RecipeRatingId { get; set; }
+        public int RecipeId { get; set; }
+        public int RecipeRating { get; set; }
+        public string RecipeComments { get; set; }
+    }
+
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public int RecipeId { get; set; }
+        public string RecipeTags { get; set; }
+        public string TagDescription { get; set; }
+    }
+}
