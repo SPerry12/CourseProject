21fff12a061a0b115f75eff89b5bd19a333e4f0b
Shane <perrysm98@gmail.com>
2020-10-13 17:08:50 -0500

    Moved html and css to ASP.NET Project....Also did revamp of html and css

---
+++ static/html/CookBookPlus/CookBookPlus/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CookBookPlus.Models;
+
+namespace CookBookPlus.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        public IActionResult RecipePage()
+        {
+            return View();
+        }
+    }
+}
